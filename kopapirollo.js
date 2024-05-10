330d2bc7c5db79f130c3ace9e662ac530f7da0f5
Zalan12 <farkaszalan@turr.hu>
2024-05-10 14:11:46 +0200

    A feladat folytatása, random generálás

--- kopapirollo.js
+++ kopapirollo.js
@@ -2,7 +2,9 @@ let kattszam1=0
 let kattszam2=0
 let kattszam3=0
 let tepontszam=0;
+let kivalasztottkep;
 let geppontszam=0;
+let eredmeny=document.getElementById("eredmeny")
 
 let tepont=document.getElementById("tepont");
 tepont.innerHTML=tepontszam;
@@ -15,13 +17,45 @@ let kep3=document.getElementById("kep3")
 
 let ellendiv=document.getElementById("ellendiv");
 let ellenkep=document.createElement("img")
-ellenkep.src="ko.png"
+
 ellendiv.appendChild(ellenkep)
+let randomszam=Math.floor(Math.random() * 3)
+
+function random()
+{
+    randomszam=Math.floor(Math.random() * 3)
+    if(randomszam==0)
+    {
+        ellenkep.src="ko.png"
+
+    }
+    else if(randomszam==1)
+    {
+        ellenkep.src="ollo.png"
+
+    }
+    else
+    {
+        ellenkep.src="Paper.png"
+
+    }
+
+}
+function dontetlen()
+{
+    if(kivalasztottkep.src==ellenkep.src)
+    {
+        eredmeny.innerHTML="Döntetlen"
+    }
+}
+
+
+
 
 
 function keret1()
 {
-    
+    kivalasztottkep=kep1.src;
     
     if(kattszam1%2==0)
     {
@@ -37,13 +71,19 @@ function keret1()
 
     kep2.style.border="none"
     kep3.style.border="none"
+
+    kattszam2=0
+    kattszam3=0
+
+    random()
+    dontetlen()
     
 
 
 }
 function keret2()
 {
-    
+    kivalasztottkep=kep2.src;
     
     if(kattszam2%2==0)
     {
@@ -57,12 +97,17 @@ function keret2()
     }
     kep1.style.border="none"
     kep3.style.border="none"
+    kattszam1=0
+    kattszam3=0
+
+    random()
+    dontetlen()
 
 }
 function keret3()
 {
     
-    
+    kivalasztottkep=kep3.src;
     if(kattszam3%2==0)
     {
     kep3.style.border="solid 3px red";
@@ -75,6 +120,12 @@ function keret3()
     }
     kep1.style.border="none"
     kep2.style.border="none"
+    kattszam1=0
+    kattszam2=0
+
+    random()
+    dontetlen()
 }
-let eredmeny=document.getElementById("eredmeny")
+
+
 
